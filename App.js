0153ec6ae5d03807d41b663ddcaddf56c42cbbfc
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-02-01 13:44:20 +0300

    Validate a form response

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const path = require('path');
 const bodyParser = require('body-parser');
+const Joi = require('joi');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public',express.static(path.join(__dirname,'static')));
-app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
   response.sendfile(path.join(__dirname,'static','index.html'));
 });
 
 app.post('/',(request,response)=>{
-  console.log(request.body);
-  // Database stuff
-  response.json({success : true});
+  const schema = Joi.object().keys({
+    email : Joi.string().trim().email().required(),
+    password : Joi.string().min(5).max(10).required()
+  });
+
+  Joi.validate(request.body,schema,(error,result)=>{
+    if(error){
+      console.log(error);
+      response.send('An error has occurred');
+    }
+      console.log(result);
+      response.send("Successfully posted");
+  });
 });
 
 app.listen(3005);
